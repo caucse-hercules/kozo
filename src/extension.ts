4b86f9a93c24a838c9194a1a5c54369f627f4b2c
dkile <dkile532@naver.com>
2022-11-24 18:02:51 +0900

    chore: remove unused hello command

--- src/extension.ts
+++ src/extension.ts
@@ -28,14 +28,7 @@ export function activate(context: vscode.ExtensionContext) {
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand(
-    "kozo.helloWorld",
-    async () => {
-      // The code you place here will be executed every time your command is executed
-      // Display a message box to the user
-      vscode.window.showInformationMessage("Hello World from kozo.bak!");
-    }
-  );
+  let disposable = vscode.commands.registerCommand("", () => {});
 
   context.subscriptions.push(disposable);
 }
