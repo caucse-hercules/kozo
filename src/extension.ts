c715605aadf1e6ce2a598ea3de3ba74a40f3bd5f
dkile <dkile532@naver.com>
2022-11-26 19:35:01 +0900

    feat: add setting kozo project config

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,7 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
 import * as fs from "fs";
+import { setKozoDefaultConfig } from "./projectSettingInput";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -28,7 +29,13 @@ export function activate(context: vscode.ExtensionContext) {
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand("", () => {});
+  let disposable = vscode.commands.registerCommand(
+    "kozo.createEmptyKozo",
+    async () => {
+      const state = await setKozoDefaultConfig(context);
+      console.log(state);
+    }
+  );
 
   context.subscriptions.push(disposable);
 }
