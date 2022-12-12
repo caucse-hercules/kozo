2dd4d4f6c976caeda10bbd9fd135293d8d428172
dkile <dkile532@naver.com>
2022-12-12 17:32:18 +0900

    feat: adjust nx

--- packages/core/src/extension.ts
+++ packages/core/src/extension.ts
@@ -10,7 +10,7 @@ export function activate(context: vscode.ExtensionContext) {
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
   console.log('Congratulations, your extension "kozo" is now active!');
-
+  const disposables: vscode.Disposable[] = [];
   let isViewsWelcomeVisible = true;
   const workspaceFolders = vscode.workspace.workspaceFolders;
   if (
@@ -29,15 +29,14 @@ export function activate(context: vscode.ExtensionContext) {
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand(
-    "kozo.createEmptyKozo",
-    async () => {
+  disposables.push(
+    vscode.commands.registerCommand("kozo.createEmptyKozo", async () => {
       const state = await setKozoDefaultConfig(context);
       console.log(state);
-    }
+    })
   );
 
-  context.subscriptions.push(disposable);
+  disposables.forEach((disposable) => context.subscriptions.push(disposable));
 }
 
 // This method is called when your extension is deactivated
